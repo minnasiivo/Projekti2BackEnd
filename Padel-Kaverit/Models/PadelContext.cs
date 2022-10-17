2f3e8c2b6885c42171a3edb0f45a623a4df1d763
Minna S <minna.siivo@student.lab.fi>
2022-10-17 09:52:44 +0300

    ??

---
+++ Padel-Kaverit/Models/PadelContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Padel_Kaverit.Models
+{
+    public class PadelContext : DbContext
+   
+    {
+           public PadelContext(DbContextOptions<PadelContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Person> Persons { get; set; }
+        
+    }
+
+}
---
+++ Padel-Kaverit/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Padel_Kaverit.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string password { get; set; }
+
+    }
+}
