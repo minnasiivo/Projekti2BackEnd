6a55b61c39839959c70b830a7757238b8be6162e
aurorasarkka <77781578+aurorasarkka@users.noreply.github.com>
2023-02-10 14:12:01 +0200

    forum + games parannuus

--- Padel-Kaverit/Repositories/ForumPostRepository.cs
+++ Padel-Kaverit/Repositories/ForumPostRepository.cs
@@ -1,4 +1,5 @@
-﻿using Padel_Kaverit.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Padel_Kaverit.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,69 @@ namespace Padel_Kaverit.Repositories
         {
             _context = context;
         }
+
+        public async Task<ForumPost> AddPostAsync(ForumPost post)
+        {
+            _context.ForumPosts.Add(post);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+            }
+            return post;
+        }
+
+        public async Task<bool> DeletePostAsync(long Id)
+        {
+            ForumPost post = _context.ForumPosts.Find(Id);
+            _context.ForumPosts.Remove(post);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            { 
+                return false; 
+            }
+
+            return true;
+        }
+
+        public async Task<IEnumerable<ForumPost>> GetAllPostAsync()
+        {
+            return await _context.ForumPosts.ToListAsync();
+        }
+
+        public async Task<IEnumerable<ForumPost>> GetPostAsync(string writer)
+        {
+
+            return await _context.ForumPosts.Include(i => i.Writer).ToListAsync();
+        }
+
+        public async Task<IEnumerable<ForumPost>> GetPostAsync(DateTime time)
+        {
+            return await _context.ForumPosts.Include(i => i.Published).ToListAsync();
+        }
+
+        public async Task<ForumPost> UpdatePostAsync(ForumPost post)
+        {
+            _context.ForumPosts.Update(post);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return post;
+        }
     }
 }
+
